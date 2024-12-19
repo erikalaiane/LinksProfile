17a5f780b1d29b976c186a26f4edc523a76a4f02
erikalaiane <>
2024-12-19 01:35:27 -0300

    adjust

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function toggleMode() {
    if(html.classList.contains("light")){
     
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
 
     } else{
         //se estiver sem light mode, manter a imagem normal
-        img.setAttribute("src", "assets/avatar.png")
+        img.setAttribute("src", "./assets/avatar.png")
     }
 }
